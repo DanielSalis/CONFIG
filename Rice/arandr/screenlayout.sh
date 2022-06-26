659603f5451a83992f6d869ca552657b34e8c1e4
Daniel Amorim Vilela de Salis <38293875+DanielSalis@users.noreply.github.com>
2022-06-26 10:36:15 -0300

    updating config

--- Rice/arandr/screenlayout.sh
+++ Rice/arandr/screenlayout.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2 --mode 1366x768 --pos 3840x0 --rotate normal --output HDMI-1 --off;
-feh --bg-scale /home/daniel/Imagens/wallpapers/jp-city-nord.png
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 3840x0 --rotate normal --output DP-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2 --off --output HDMI-1 --off && feh --bg-scale /home/daniel/Imagens/wallpapers/031.png
