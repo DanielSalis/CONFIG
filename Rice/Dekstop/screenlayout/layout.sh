62babb999ffa738677da0b8980295bcaf5f8df95
Daniel Amorim Vilela de Salis <38293875+DanielSalis@users.noreply.github.com>
2022-08-05 08:07:49 -0300

    add screenlayout

---
+++ Rice/Dekstop/screenlayout/layout.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output HDMI-0 --primary --mode 1920x1080 --rate 144 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off &
+feh --bg-scale /home/daniel/Pictures/wallpapers/tokyo-night-flower.png
