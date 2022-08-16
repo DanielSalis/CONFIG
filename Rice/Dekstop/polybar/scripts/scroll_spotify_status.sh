5c997aab9fbec93ee0d1ef6787f059eeb7694d00
Daniel Amorim Vilela de Salis <38293875+DanielSalis@users.noreply.github.com>
2022-08-16 18:04:21 -0300

    update zscroll

--- Rice/Dekstop/polybar/scripts/scroll_spotify_status.sh
+++ Rice/Dekstop/polybar/scripts/scroll_spotify_status.sh
@@ -5,7 +5,7 @@ zscroll -l 30 \
         --delay 0.1 \
         --scroll-padding "  " \
         --match-command "`dirname $0`/get_spotify_status.sh --status" \
-        --match-text "Playing" "--scroll 1" \
+        --match-text "Playing" "--scroll 0" \
         --match-text "Paused" "--scroll 0" \
         --update-check true "`dirname $0`/get_spotify_status.sh" &
 
