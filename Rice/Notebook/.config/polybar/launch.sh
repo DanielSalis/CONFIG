05a98ee8cb0732de81a9a53f586f65f90592852b
Daniel Amorim Vilela de Salis <38293875+DanielSalis@users.noreply.github.com>
2022-08-18 09:43:35 -0300

    update

--- Rice/Notebook/.config/polybar/launch.sh
+++ Rice/Notebook/.config/polybar/launch.sh
@@ -8,6 +8,6 @@ killall -q polybar
 
 # Launch bar1 and bar2
 polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
-# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar eDP 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
