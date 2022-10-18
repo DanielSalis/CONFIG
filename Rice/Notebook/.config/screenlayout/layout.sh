245424a3b0d671eb8d8c4c0c791d86c41047aab4
Daniel Amorim Vilela de Salis <38293875+DanielSalis@users.noreply.github.com>
2022-10-18 08:11:41 -0300

    .

--- Rice/Notebook/.config/screenlayout/layout.sh
+++ Rice/Notebook/.config/screenlayout/layout.sh
@@ -6,7 +6,8 @@ SECOND_MONITOR=`xrandr -q | grep -i '^DP-1'`
 if [[ $SECOND_MONITOR =~ "disconnected" ]]; then
   xrandr --output eDP-1 --primary --mode 1920x1080 && feh --bg-scale /home/daniel/Imagens/wallpapers/tokyo-night-flower.png
 else
-  xrandr --output eDP-1 --primary --mode 1920x1080 --pos 3840x0 --rotate normal --output DP-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2 --off --output HDMI-1 --off && feh --bg-scale /home/daniel/Imagens/wallpapers/tokyo-night-flower.png
+  # xrandr --output eDP-1 --primary --mode 1920x1080 --pos 3840x0 --rotate normal --output DP-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2 --off --output HDMI-1 --off && feh --bg-scale /home/daniel/Imagens/wallpapers/tokyo-night-flower.png
+  xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1366x0 --rotate normal --output DP-1 --mode 1366x768 --pos 0x0 --rotate normal --output DP-2 --off --output HDMI-1 --off && feh --bg-scale /home/daniel/Imagens/wallpapers/tokyo-night-flower.png
 fi
 
 echo $SECOND_MONITOR
